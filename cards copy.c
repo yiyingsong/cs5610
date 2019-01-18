7a92233f51f75d20d52e4f174cf365922e563994
sophiesong <song.yiy@husky.neu.edu>
2019-01-17 20:21:52 -0800

    upload test file

---
+++ cards copy.c
@@ -0,0 +1,18 @@
+//
+//  cards.c
+//  
+//
+//  Created by Sophie Song on 1/16/19.
+//
+
+#include "cards.h"
+#include <stdio.h>
+
+int main() {
+    puts("Enter the name: ");
+    char name[10];
+    scanf("%s", name);
+    printf("Hello %s!", name);
+    return 0;
+}
+
